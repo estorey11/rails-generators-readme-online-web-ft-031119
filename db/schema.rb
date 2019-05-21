7c17b3e74dafa0f350b7cbccfc66c1932082e1c8
Eric Storey <estorey@gmail.com>
2019-05-21 21:25:50 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190521211931) do
+ActiveRecord::Schema.define(version: 20190521212525) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20190521211931) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.         "post_status"
   end
 
 end
